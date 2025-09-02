8f7504eb95e7948def96066cdc0d22aebb99fbcf
yumoqing <yumoqing@gmail.com>
2025-09-02 17:36:15 +0800

    bugfix

--- llmage/llmclient.py
+++ llmage/llmclient.py
@@ -76,9 +76,10 @@ async def uapi_request(request, sor, caller_orgid, callerid, uapi, llm, params):
 	userid = await get_owner_userid(sor, llm)
 	txt = ''
 	async for l in uapi.stream_linify(llm.upappid, llm.apiname, userid, params=params):
-		d = json.loads(l)
-		txt = txt + d['content']
-		yield l
+		if l != '[DONE]':
+			d = json.loads(l)
+			txt = txt + d['content']
+			yield l
 	debug(f'{d=}, {txt=}')
 	
 async def inference(request, *args, **kw):
