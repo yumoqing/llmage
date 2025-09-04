a8d69eeefea1e736c769ddda9b3daf2750f0b5eb
yumoqing <yumoqing@gmail.com>
2025-09-04 17:07:11 +0800

    bugfix

--- llmage/init.py
+++ llmage/init.py
@@ -1,5 +1,5 @@
 from llmage.llmclient import (
-	b64media,
+	b64media2url,
 	get_llm, 
 	inference, 
 	get_llmcatelogs,
@@ -17,7 +17,7 @@ from ahserver.serverenv import ServerEnv
 def load_llmage():
 	env = ServerEnv()
 	env.get_llm = get_llm
-	env.b64media = b64media
+	env.b64media2url = b64media2url
 	env.inference = inference
 	env.get_llms_by_catelog = get_llms_by_catelog
 	env.get_llmcatelogs = get_llmcatelogs
