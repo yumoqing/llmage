df15135db73006ceea6741ed867bf25bdafa28ee
yumoqing <yumoqing@gmail.com>
2025-09-04 18:01:27 +0800

    bugfix

--- llmage/llmclient.py
+++ llmage/llmclient.py
@@ -98,7 +98,7 @@ async def uapi_request(request, sor, caller_orgid, callerid, uapi, llm, params):
 	
 def b64media2url(request, mediafile):
 	env = request._run_ns
-	debug('{request=}, {env=},{type(env)}, {len(mediafile)=}')
+	debug(f'{request=}, {env=},{type(env)}, {len(mediafile)=}')
 	if mediafile.startswith('data:'):
 		fs = FileStorage()
 		fname = getFilenameFromBase64(mediafile)
