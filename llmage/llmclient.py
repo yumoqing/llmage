4914ddf4dd862dfcb22b8602950747e05a3076f5
yumoqing <yumoqing@gmail.com>
2025-09-08 19:08:31 +0800

    bugfix

--- llmage/llmclient.py
+++ llmage/llmclient.py
@@ -132,7 +132,7 @@ async def async_uapi_request(request, llm, sor):
 			b = b.decode('utf-8')
 		rzt = DictObject(**json.loads(b))
 		yield b + '\n'
-		if not rzt.taskid:
+		if not rzt.status:
 			debug(f'{b=} return error')
 			return
 		if rzt.url:
