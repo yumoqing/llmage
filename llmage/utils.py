f16e6761f4d19cf0f93f9440a25c295617454467
yumoqing <yumoqing@gmail.com>
2026-04-03 14:23:18 +0800

    bugfix

--- llmage/utils.py
+++ llmage/utils.py
@@ -14,7 +14,7 @@ from uapi.appapi import UAPI, sor_get_callerid, sor_get_uapi
 from ahserver.serverenv import get_serverenv, ServerEnv
 from ahserver.filestorage import FileStorage
 
-async def query_order(userorgid):
+async def llm_query_order(userorgid):
 	env = ServerEnv()
 	async with get_sor_context(env, 'llmage') as sor:
 		recs = await sor.R('llmusage', {'userorgid': userorgid})
