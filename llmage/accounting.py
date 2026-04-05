484d4c99deae82ae19dc957cf244c02c75953cef
yumoqing <yumoqing@gmail.com>
2026-04-05 16:06:49 +0800

    bugfix

--- llmage/accounting.py
+++ llmage/accounting.py
@@ -164,6 +164,7 @@ where a.llmid = b.id
 		if luid:
 			sql += " and a.id=${luid}$"
 			ns['luid'] = luid
+		debug(f'{sql=}, {ns=}')
 		recs = await sor.sqlExe(sql, ns)
 		for r in recs:
 			if r.usages is None:
