a739979d2e200b5b8a31476d61497e3a78beaf12
yumoqing <yumoqing@gmail.com>
2026-04-09 15:19:49 +0800

    bugfix

--- llmage/asyncinference.py
+++ llmage/asyncinference.py
@@ -85,10 +85,10 @@ async def async_uapi_request(request, llm,
 		llmusage.use_date = curDateString()
 		llmusage.use_time = timestampstr()
 		llmusage.userid = callerid
-		ioinfo = json.dumps({
+		ioinfo = {
 			"input": params_kw,
 			'output': [d]
-		}, ensure_ascii=False)
+		}
 		webpath = await write_llmio(llmusage.id, ioinfo)
 		llmusage.ioinfo = webpath
 		llmusage.taskid = d.taskid
