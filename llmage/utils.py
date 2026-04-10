a0ad8af80ff3bfc99effac81b3566c56e98945cb
yumoqing <yumoqing@gmail.com>
2026-04-10 14:50:11 +0800

    bugfix

--- llmage/utils.py
+++ llmage/utils.py
@@ -26,7 +26,7 @@ async def append_new_llmoutput(webpath, output):
 		iostr = json.dumps(io, ensure_ascii=False, indent=4)
 		f.write(iostr.encode('utf-8'))
 	
-async def get_usage_from_lastoutput(webpath):
+async def get_lastoutput(webpath):
 	bin =  await read_webpath(webpath)
 	io = json.load(bin.decode('utf-8'))
 	return io['output'][-1]
