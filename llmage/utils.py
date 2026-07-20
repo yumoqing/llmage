ee68fbefa77487188ad46bb018fb2c300c3e0df7
yumoqing <yumoqing@gmail.com>
2026-07-20 19:19:47 +0800

    bugfix

--- llmage/utils.py
+++ llmage/utils.py
@@ -360,8 +360,7 @@ m.ppid
 			and m.ppid is not null
 			and a.expired_date > ${today}$
 			"""
-		sortstr='catelog_id, ' + orderby
-		params = {'today': today, 'sort': sortstr}
+		params = {'today': today, 'sort': ['catelog_id', 'providerid', 'name'}
 		if catelogid:
 			sql += " and m.llmcatelogid = ${catelogid}$"
 			params['catelogid'] = catelogid
